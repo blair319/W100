1fb0573e430b7b586bb7c563ec3effef3c674889
chenchangyan0222@126.com <chenchangyan0222@126.com>
2018-05-08 23:18:36 +0800

    修改图片路径

--- app/controller/v1/aid/config.js
+++ app/controller/v1/aid/config.js
@@ -17,8 +17,8 @@ class ConfigController extends Controller {
         ctx.body = {
             code: 0,
             data: [
-                { "type": 0, "imgurl": "http://chuantu.biz/t6/297/1524905116x-1404795690.png", deturl: "", id: "134e" },
-                { "type": 1, "imgurl": "http://chuantu.biz/t6/297/1524905116x-1404795690.png", deturl: "http://www.baidu.com", id: "34e2" },
+                { "type": 0, "imgurl": "http://chuantu.biz/t6/305/1525767305x-1404792891.png", deturl: "", id: "134e" },
+                { "type": 1, "imgurl": "http://chuantu.biz/t6/305/1525767334x-1404792891.png", deturl: "http://www.baidu.com", id: "34e2" },
                 { "type": 1, "imgurl": "http://chuantu.biz/t6/297/1524905116x-1404795690.png", deturl: "http://www.baidu.com", id: "we3" },
             ],
             message: "OK",
